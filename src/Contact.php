26cdb1dc867b57e343ba32ad6e6e65e73bdeaca9
danlauby <dmlauby@gmail.com>
2017-02-10 09:35:38 -0800

    Add saveContact, getAllContacts(static), deleteAllContacts(static) functions to Contact class and fix indentation in body of functions

--- src/Contact.php
+++ src/Contact.php
@@ -1,45 +1,64 @@
 <?php
     class Contact
-    {
+    {   // Contact properties
         private $name;
         private $address;
         private $phone_number;
 
+        // Contact constructor
         function __construct($new_name, $new_address, $new_phone_number)
         {
-        $this->name = $new_name;
-        $this->address = $new_address;
-        $this->phone_number = $new_phone_number;
+            $this->name = $new_name;
+            $this->address = $new_address;
+            $this->phone_number = $new_phone_number;
         }
 
+        // Setters and Getters for Contact properties
         function setName($new_name)
         {
-        $this->name = (string) $new_name;
+            $this->name = (string) $new_name;
         }
 
         function getName()
         {
-        return $this->name;
+            return $this->name;
         }
 
         function setAddress($new_address)
         {
-        $this->address = (string) $new_address;
+            $this->address = (string) $new_address;
         }
 
         function getAddress()
         {
-        return $this->address;
+            return $this->address;
         }
 
         function setPhoneNumber($new_phone_number)
         {
-        $this->phone_number = (string) $new_phone_number;
+            $this->phone_number = (string) $new_phone_number;
         }
 
         function getPhoneNumber()
         {
-        return $this->phone_number;
+            return $this->phone_number;
         }
+
+        function saveContact()
+        {
+            array_push($_SESSIONS['list_of_contacts'], $this);
+        }
+
+        // Super-global functions
+        static function getAllContacts()
+        {
+            return $_SESSION['list_of_contacts'];
+        }
+
+        static function deleteAllContacts()
+        {
+            return $_SESSION['list_of_contacts'] = [];
+        }
+
     }
 ?>
